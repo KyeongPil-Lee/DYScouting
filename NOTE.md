345ecf5a2e6a770ac9017ed22d09dbd9e5a4a54c
kplee <kplee@cern.ch>
2019-10-10 14:39:55 +0900

    initialize

---
+++ NOTE.md
@@ -0,0 +1,10 @@
+# DY analysis with the scouting data
+
+## Setup
+
+```
+git clone git@github.com:KyeongPil-Lee/DYScouting.git
+cd DYScouting
+source setup.sh
+```
+
---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ $DY_ANALYZER_PATH ]; then
+    echo "DY_ANALYZER_PATH is already defined: use a clean shell!"
+    return 1
+fi
+
+export DY_ANALYZER_PATH=$(pwd)
+
+# -- root setup -- #
+export ROOT_INCLUDE_PATH=${DY_ANALYZER_PATH}:${ROOT_INCLUDE_PATH}
+export PYTHONPATH=${DY_ANALYZER_PATH}:${PYTHONPATH}
+
+export DY_NTUPLE_PATH=""
+# if [ $HOSTNAME == "tamsa2.snu.ac.kr" ]; then
+#   DY_NTUPLE_PATH="/data9/DATA/DYntuple"
+
+# elif [ $HOSTNAME == "muon" ]; then
+#   DY_NTUPLE_PATH="/scratch/kplee/DYntuple"
+
+# elif [ $USER == "KyeongPil_Lee" ]; then
+#   DY_NTUPLE_PATH="/Users/KyeongPil_Lee/temp/DYntuple"
+
+# fi
+echo "========================================"
+echo "Enviornment variables"
+echo "DY_ANALYZER_PATH: "$DY_ANALYZER_PATH
+echo "DY_NTUPLE_PATH:   "$DY_NTUPLE_PATH
+echo "Setup for DY analyzer is finished."
+echo "========================================"
