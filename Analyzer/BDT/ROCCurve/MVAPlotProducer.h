23845cdb9c4fa60d3b6e128601966d6d31238b81
kplee <kplee@cern.ch>
2020-02-11 22:21:39 +0900

    to draw MVA distribution for DY and QCD

---
+++ Analyzer/BDT/ROCCurve/MVAPlotProducer.h
@@ -0,0 +1,128 @@
+#include <TChain.h>
+#include <TFile.h>
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TLorentzVector.h>
+#include <TStopwatch.h>
+#include <TTimeStamp.h>
+#include <TString.h>
+#include <TLegend.h>
+#include <THStack.h>
+#include <TPad.h>
+#include <TCanvas.h>
+#include <TColor.h>
+#include <TAttMarker.h>
+#include <TF1.h>
+#include <TStyle.h>
+#include <TEfficiency.h>
+
+#include <vector>
+
+#include <Include/DYTool.h>
+#include <BDT/dataset/weights/TMVAClassification_BDTG.class.C>
+
+class HistProducer: public DYTool::ClassTemplate
+{
+public:
+  HistProducer(): ClassTemplate()
+  {
+
+  }
+
+  void Run()
+  {
+    CheckSampleInfo();
+    StartTimer();
+
+    TChain *chain = new TChain("DYTree/ntuple");
+    DYTool::AddNtupleToChain(chain, sampleInfo_.ntuplePathFile);
+
+    DYTool::DYTree *ntuple = new DYTool::DYTree( chain );
+
+    Int_t nEvent = chain->GetEntries();
+    cout << "\t[Total Events: " << nEvent << "]" << endl;
+
+    DYTool::PUReweightTool* PUTool = new DYTool::PUReweightTool("2018");
+
+    TH1D* h_MVA         = new TH1D("h_MVA",         "", 2000, -1, 1);
+    TH1D* h_MVA_ZPeak   = new TH1D("h_MVA_ZPeak",   "", 2000, -1, 1);
+    TH1D* h_MVA_M10to60 = new TH1D("h_MVA_M10to60", "", 2000, -1, 1);
+
+    vector<std::string> vec_inputVarName = {
+      "diMuNormVtxChi2", "diMuDEta", "diMuDPhi", 
+      "caloMET_pt", 
+      "nMuonHit_lead", "nMatchedStation_lead", "nPixelHit_lead", "nTrackerLayer_lead", "normChi2_lead", "relTrkIso_lead", 
+      "nMuonHit_sub", "nMatchedStation_sub", "nPixelHit_sub", "nTrackerLayer_sub", "normChi2_sub", "relTrkIso_sub"
+    };
+
+    ReadBDTG* BDTClassifier = new ReadBDTG(vec_inputVarName);
+
+    for(Int_t i=0; i<nEvent; i++)
+    {
+      DYTool::loadBar(i+1, nEvent, 100, 100);
+      
+      ntuple->GetEvent(i);
+
+      Double_t genWeight;
+      ntuple->genWeight < 0 ? genWeight = -1 : genWeight = 1;
+
+      // Double_t totWeight = sampleInfo_.normFactor * genWeight;
+      Double_t totWeight = genWeight;
+      if( sampleInfo_.isMC ) totWeight = genWeight * PUTool->Weight( ntuple->truePU );
+
+      // -- only DY->mumu or DY->ee events according to its name -- //
+      if( DYTool::SelectGenEventBySampleType(sampleInfo_.type, ntuple) )
+      {
+        Bool_t isDYEvent = kFALSE;
+        DYTool::MuPair DYMuPair = DYTool::EventSelection_BDTInput(ntuple, isDYEvent);
+        if( isDYEvent )
+        {
+          vector<Double_t> vec_BDTInputVar = {
+            DYMuPair.normVtxChi2, DYMuPair.dEta, DYMuPair.dPhi,
+            ntuple->caloMET_pt,
+            (Double_t)DYMuPair.first_.nMuonHit, (Double_t)DYMuPair.first_.nMatchedStation, (Double_t)DYMuPair.first_.nPixelHit, (Double_t)DYMuPair.first_.nTrackerLayer, DYMuPair.first_.normChi2, DYMuPair.first_.relTrkIso,
+            (Double_t)DYMuPair.second_.nMuonHit, (Double_t)DYMuPair.second_.nMatchedStation, (Double_t)DYMuPair.second_.nPixelHit, (Double_t)DYMuPair.second_.nTrackerLayer, DYMuPair.second_.normChi2, DYMuPair.second_.relTrkIso
+          };
+
+          Double_t mvaValue = BDTClassifier->GetMvaValue( vec_BDTInputVar );
+
+          Double_t mass = DYMuPair.mass;
+          if( mass > 10 )
+            h_MVA->Fill( mvaValue, totWeight );
+
+          if( 60 < mass && mass < 120 )
+            h_MVA_ZPeak->Fill( mvaValue, totWeight );
+
+          if( 10 < mass && mass < 60 )
+            h_MVA_M10to60->Fill(mvaValue, totWeight);
+        }
+      }
+    }
+
+    TString outputName = TString::Format("ROOTFile_MVAPlot_%s.root", sampleInfo_.type.Data());
+    TFile *f_output = TFile::Open(outputName, "RECREATE");
+
+    h_MVA->Write();
+    h_MVA_ZPeak->Write();
+    h_MVA_M10to60->Write();
+
+    f_output->Close();
+
+    PrintRunTime();
+  }
+
+};
+
+void Test()
+{
+  HistProducer* producer = new HistProducer();
+
+  producer->sampleInfo_.type = "DYMuMu_M10to50_NLOXSec_TEST";
+  producer->sampleInfo_.ntuplePathFile = "/Users/KyeongPil_Lee/Physics/DYScouting/Analyzer/Include/Example/ntuplePath_example.txt";
+  producer->sampleInfo_.xSec = 10000;
+  producer->sampleInfo_.sumWeight = 10000;
+  Double_t lumi = 999;
+  producer->sampleInfo_.normFactor = (lumi * 10000.0 ) / 10000.0;
+
+  producer->Run();
+}
