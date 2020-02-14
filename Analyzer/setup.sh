fc71b7101abf890d638e34cd567ba22d3276199e
kplee <kplee@cern.ch>
2020-02-14 13:44:45 +0900

    add cmsenv for tamsa2

--- Analyzer/setup.sh
+++ Analyzer/setup.sh
@@ -22,6 +22,15 @@ elif [ $HOSTNAME == "muon" ]; then
 #   DY_NTUPLE_PATH="/Users/KyeongPil_Lee/temp/DYntuple"
 
 fi
+
+if [ $HOSTNAME == "tamsa2" ]; then
+  source /cvmfs/cms.cern.ch/cmsset_default.sh
+  cd /cvmfs/cms.cern.ch/slc7_amd64_gcc700/cms/cmssw/CMSSW_10_2_6
+  eval `scramv1 runtime -sh`
+  cd $DY_ANALYZER_PATH
+  echo "CMSSW enviornment under " $CMSSW_VERSION
+fi
+
 echo "========================================"
 echo "Enviornment variables"
 echo "DY_ANALYZER_PATH: "$DY_ANALYZER_PATH
