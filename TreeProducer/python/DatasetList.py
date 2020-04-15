b8406f011a1e06a790c92618eaf9d188bd97770d
Kyeongpil Lee <kplee@cern.ch>
2020-04-15 12:58:07 +0900

    list of the dataset

---
+++ TreeProducer/python/DatasetList.py
@@ -0,0 +1,38 @@
+
+
+
+dic_tag_dataset_data = {
+    "ScoutingCaloMuon_Run2018Av1_GoldenJSON" : "/ScoutingCaloMuon/Run2018A-v1/RAW",
+    "ScoutingCaloMuon_Run2018Bv1_GoldenJSON" : "/ScoutingCaloMuon/Run2018B-v1/RAW",
+    "ScoutingCaloMuon_Run2018Cv1_GoldenJSON" : "/ScoutingCaloMuon/Run2018C-v1/RAW",
+    "ScoutingCaloMuon_Run2018Dv1_GoldenJSON" : "/ScoutingCaloMuon/ScoutingCaloMuon_Run2018Cv1_GoldenJSON-v1/RAW",
+}
+
+dic_tag_dataset_MC = {
+    "DYJetsToLL_M10to50_Madgraph"     : "/DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v2/AODSIM",
+    "DYJetsToLL_M10to50_Madgraph_ext" : "/DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15_ext1-v1/AODSIM",
+    "DYJetsToLL_M50toInf_aMCNLO"      : "/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM",
+    "DYJetsToLL_M50toInf_aMCNLO_ext"  : "/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15_ext2-v1/AODSIM",
+    "QCDMuEnriched_Pt20toInf"         : "/QCD_Pt-20toInf_MuEnrichedPt15_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM",
+    "QCDMuEnriched_Pt15to20"          : "/QCD_Pt-15to20_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v3/AODSIM",
+    "QCDMuEnriched_Pt20to30"          : "/QCD_Pt-20to30_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v4/AODSIM",
+    "QCDMuEnriched_Pt30to50"          : "/QCD_Pt-30to50_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v3/AODSIM",
+    "QCDMuEnriched_Pt50to80"          : "/QCD_Pt-50to80_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v3/AODSIM",
+    "QCDMuEnriched_Pt80to120"         : "/QCD_Pt-80to120_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM",
+    "QCDMuEnriched_Pt80to120_ext"     : "/QCD_Pt-80to120_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15_ext1-v2/AODSIM",
+    "QCDMuEnriched_Pt120to170"        : '/QCD_Pt-120to170_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM',
+    "QCDMuEnriched_Pt120to170_ext"    : "/QCD_Pt-120to170_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15_ext1-v2/AODSIM'",
+    "QCDMuEnriched_Pt170to300"        : '/QCD_Pt-170to300_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v3/AODSIM',
+    "QCDMuEnriched_Pt300to470"        : '/QCD_Pt-300to470_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v3/AODSIM',
+    "QCDMuEnriched_Pt300to470_ext"    : '/QCD_Pt-300to470_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15_ext3-v1/AODSIM',
+    "QCDMuEnriched_Pt470to600"        : '/QCD_Pt-470to600_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM',
+    "QCDMuEnriched_Pt470to600_ext"    : '/QCD_Pt-470to600_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15_ext1-v2/AODSIM',
+    "QCDMuEnriched_Pt600to800"        : '/QCD_Pt-600to800_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM',
+    "QCDMuEnriched_Pt800to1000"       : '/QCD_Pt-800to1000_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15_ext3-v2/AODSIM',
+    "QCDMuEnriched_Pt1000toInf"       : '/QCD_Pt-1000toInf_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM',
+    "TTTo2L2Nu_Powheg"                : '/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM',
+    "WJetsToLNu_Madgraph"             : '/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v2/AODSIM',
+}
+
+dic_tag_dataset_all = dic_tag_dataset_data
+dic_tag_dataset_all.update( dic_tag_dataset_MC ) # -- merge
