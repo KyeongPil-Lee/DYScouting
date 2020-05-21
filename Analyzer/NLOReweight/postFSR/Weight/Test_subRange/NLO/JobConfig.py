5a7f811460e137d79c2d992a5642f7496dd97397
kplee <kplee@cern.ch>
2020-05-21 14:17:45 +0900

    only see 10 < m < 11 GeV

---
+++ Analyzer/NLOReweight/postFSR/Weight/Test_subRange/NLO/JobConfig.py
@@ -0,0 +1,32 @@
+import os
+
+def GetCWD():
+  cwd_fullPath = os.getcwd()
+  analyzerPath = os.environ["DY_ANALYZER_PATH"]
+  cwd = cwd_fullPath.split(analyzerPath+"/")[1] # -- start from the analyzer base path
+
+  return cwd
+
+
+from Include.MultiJobGenerator import *
+
+generator = MultiJobGenerator()
+
+generator.jobType = "HTCondor"
+generator.jobName = "NLOReweight" # -- used for workspace name
+generator.classCodePath = GetCWD()+"/MakeGenHist_DiMuPtRap_NLO.h"
+generator.className = "HistProducer"
+
+generator.luminosity = 1.0 # -- not used for now
+generator.jsonName = "SampleInfo_v1p2.json" # -- under DY_ANALYZER_PATH/Include
+
+generator.dic_nJob = { 
+  "DYMuMu_M10to50_2016":    50,
+  # "DYMuMu_M50toInf":   10,
+  }
+
+# generator.outputPath = "/scratch/kplee/DYScoutingJob"
+generator.outputPath = "/data9/Users/kplee/DYScoutingJob"
+generator.bringOutput = True # -- bring output root file to the current working directory where JobConfig is executed
+
+generator.Generate()
