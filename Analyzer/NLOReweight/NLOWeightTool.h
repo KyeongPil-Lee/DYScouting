0a3d39118a9c206ebb8f9170e408235dde0fce0c
kplee <kplee@cern.ch>
2020-08-27 16:44:38 +0900

    add 0-10 bin for the weights

--- Analyzer/NLOReweight/NLOWeightTool.h
+++ Analyzer/NLOReweight/NLOWeightTool.h
@@ -20,7 +20,7 @@ public:
     cout << "---> root file path = " << rootFilePath << endl;
 
     vector<Double_t> vec_temp = {
-      10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+      0, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
       23, 26, 29, 32, 35, 38, 41, 44, 47, 50};
     vec_massBinEdge_ = vec_temp;
 
