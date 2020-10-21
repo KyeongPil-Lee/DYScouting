7521c55a485f13ce9a2fde8ecbc0d9440d52f86c
Kyeongpil Lee <kplee@Kyeongpils-MacBook-Pro.local>
2020-10-22 00:07:53 +0900

    additional samples

---
+++ TreeProducer/CRAB/v04_v1p3/crabcfg_2018MC_Additional.py
@@ -0,0 +1,58 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = ''
+config.General.workArea = 'CRABDir'
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '../../test/ProduceTree.py'
+config.JobType.pyCfgParams = ['sampleType=MC2018']
+
+config.Data.inputDataset = ''
+
+config.Data.inputDBS = 'global'
+# config.Data.splitting = 'Automatic'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 5
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = False
+
+# config.Site.storageSite = 'T3_KR_KISTI'
+config.Site.storageSite = 'T2_KR_KNU'
+
+# config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
+# FirstRun = 271036
+# LastRun = 284044
+# config.Data.runRange = '%d-%d' % (FirstRun, LastRun)
+
+
+version = '_v20201022_'
+# 'MultiCRAB' part
+if __name__ == '__main__':
+    
+    from CRABAPI.RawCommand import crabCommand
+
+    # -- Single top
+    config.General.requestName = 'DYTree'+version+'tW_top_Powheg'
+    config.Data.inputDataset = '/ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15_ext1-v1/AODSIM'
+    crabCommand('submit', config = config)
+
+    # -- Single anti-top
+    config.General.requestName = 'DYTree'+version+'tW_antitop_Powheg'
+    config.Data.inputDataset = '/ST_tW_antitop_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15_ext1-v1/AODSIM'
+    crabCommand('submit', config = config)
+
+    # -- WW, 2 lepton & 2 neutrino
+    config.General.requestName = 'DYTree'+version+'WW2L2Nu_Powheg'
+    config.Data.inputDataset = '/WWTo2L2Nu_NNPDF31_TuneCP5_13TeV-powheg-pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM'
+    crabCommand('submit', config = config)
+
+    # -- WZ
+    config.General.requestName = 'DYTree'+version+'WZ_Pythia8'
+    config.Data.inputDataset = '/WZ_TuneCP5_PSweights_13TeV-pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM'
+    crabCommand('submit', config = config)
+
+    # -- ZZ
+    config.General.requestName = 'DYTree'+version+'ZZ_Pythia8'
+    config.Data.inputDataset = '/ZZ_TuneCP5_13TeV-pythia8/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v2/AODSIM'
+    crabCommand('submit', config = config)
