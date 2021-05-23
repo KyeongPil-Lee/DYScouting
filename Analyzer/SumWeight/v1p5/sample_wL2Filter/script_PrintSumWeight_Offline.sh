fa5e73c3c9700cd5137d8c1ef80a4b8761387faf
Kyeongpil Lee <kplee@Kyeongpils-MacBook-Pro.local>
2021-05-23 17:04:22 +0900

    test sum(weight)

---
+++ Analyzer/SumWeight/v1p5/sample_wL2Filter/script_PrintSumWeight_Offline.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_DYMuMu_M10to50.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_DYMuMu_M50toInf.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_DYTauTau_M10to50.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_DYTauTau_M50toInf.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_QCDMuEnriched_Pt1000toInf.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_QCDMuEnriched_Pt120to170.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_QCDMuEnriched_Pt15to20.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_QCDMuEnriched_Pt170to300.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_QCDMuEnriched_Pt20to30.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_QCDMuEnriched_Pt300to470.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_QCDMuEnriched_Pt30to50.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_QCDMuEnriched_Pt470to600.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_QCDMuEnriched_Pt50to80.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_QCDMuEnriched_Pt600to800.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_QCDMuEnriched_Pt800to1000.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_QCDMuEnriched_Pt80to120.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_WJets.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_Offline_ttbar.root Print_SumWeight.cxx
