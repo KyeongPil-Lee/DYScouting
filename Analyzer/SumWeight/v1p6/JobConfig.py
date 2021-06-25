352fe2a4c9519c017303d30c962acabfe687f95f
KyeongPil-Lee <kplee@cern.ch>
2021-06-25 13:05:29 +0900

    result

--- Analyzer/SumWeight/v1p6/JobConfig.py
+++ Analyzer/SumWeight/v1p6/JobConfig.py
@@ -22,8 +22,8 @@ generator.jsonName = "SampleInfo_v1p6.json" # -- under DY_ANALYZER_PATH/Include
 # -- if "_skim" is added, it runs over skimmed ntuples
 generator.dic_nJob = { 
   # "Offline_SingleMuon_Run2018All": 300,
-  "Offline_DYMuMu_M10to50":    10, 
-  "Offline_DYMuMu_M50toInf":   10,
+  # "Offline_DYMuMu_M10to50":    10, 
+  # "Offline_DYMuMu_M50toInf":   10,
   # "Offline_DYTauTau_M10to50":  3, 
   # "Offline_DYTauTau_M50toInf": 10, 
   # "Offline_WJets": 1,
@@ -41,10 +41,10 @@ generator.dic_nJob = {
   # "Offline_QCDMuEnriched_Pt800to1000": 1,
   # "Offline_QCDMuEnriched_Pt1000toInf": 1,
 
-  # "DYMuMu_M10to50":    3, 
-  # "DYMuMu_M50toInf":   10,
-  # "DYTauTau_M10to50":  3, 
-  # "DYTauTau_M50toInf": 10, 
+  "DYMuMu_M10to50":    10, 
+  "DYMuMu_M50toInf":   10,
+  "DYTauTau_M10to50":  10, 
+  "DYTauTau_M50toInf": 10, 
   # "WJets": 2,
   # "ttbar": 10,
 
