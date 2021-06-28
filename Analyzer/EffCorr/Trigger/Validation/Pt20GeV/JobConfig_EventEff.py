760d96fd3b0bfd2026152daacd39f14a1cf8e0d5
Kyeongpil Lee <kplee@Kyeongpils-MacBook-Pro.local>
2021-06-28 22:06:37 +0900

    test with pT > 20 GeV

---
+++ Analyzer/EffCorr/Trigger/Validation/Pt20GeV/JobConfig_EventEff.py
@@ -0,0 +1,30 @@
+import os
+
+def GetCWD():
+  cwd_fullPath = os.getcwd()
+  analyzerPath = os.environ["DY_ANALYZER_PATH"]
+  cwd = cwd_fullPath.split(analyzerPath+"/")[1] # -- start from the analyzer base path
+
+  return cwd
+
+
+from Include.CondorJobGenerator import *
+
+generator = MultiCondorJobGenerator()
+
+generator.jobName = "EffCorr" # -- used for workspace name
+generator.ROOTCodePath = GetCWD()+"/MakeHist_EventEff_FromTnPLegEff.h"
+generator.className = "HistProducer"
+
+generator.luminosity = 59740.565202 # -- 2018
+generator.jsonName = "SampleInfo_v1p6.json" # -- under DY_ANALYZER_PATH/Include
+
+# -- if "_skim" is added, it runs over skimmed ntuples
+generator.dic_nJob = { 
+  # "DYMuMu_M10to50":    10, 
+  "DYMuMu_M50toInf":   10,
+  }
+
+generator.baseWSPath = "/data9/Users/kplee/DYScoutingJob"
+
+generator.Generate()
---
+++ Analyzer/EffCorr/Trigger/Validation/Pt20GeV/TriggerEff_MCTruth.h
@@ -0,0 +1,112 @@
+#include <Test/SingleMuon/OfflineHistContainer.h>
+#include <NLOReweight/NLOWeightTool.h>
+
+class HistProducer: public DYTool::ClassTemplate
+{
+public:
+  HistProducer(): ClassTemplate()
+  {
+
+  }
+
+  void Run()
+  {
+    CheckSampleInfo();
+    StartTimer();
+
+    TChain *chain = new TChain("DYTree/ntuple");
+    DYTool::AddNtupleToChain(chain, sampleInfo_.ntuplePathFile);
+
+    DYTool::DYTree *ntuple = new DYTool::DYTree( chain );
+    ntuple->TurnOnBranches_OffMuon();
+
+    Int_t nEvent = chain->GetEntries();
+    cout << "\t[Total Events: " << nEvent << "]" << endl;
+
+    DYTool::PUReweightTool* PUTool = new DYTool::PUReweightTool("2018");
+    NLOWeightTool* nloWeightTool = new NLOWeightTool();
+
+    TH1D* h_diMuM_DEN = new TH1D("h_diMuM_DEN", "", 150, 50, 200);
+    TH1D* h_diMuM_NUM = new TH1D("h_diMuM_NUM", "", 150, 50, 200);
+
+    for(Int_t i=0; i<nEvent; i++)
+    {
+      DYTool::loadBar(i+1, nEvent, 100, 100);
+      
+      ntuple->GetEvent(i);
+
+      Double_t genWeight;
+      ntuple->genWeight < 0 ? genWeight = -1 : genWeight = 1;
+
+      Double_t totWeight = sampleInfo_.normFactor * genWeight;
+      // Double_t totWeight = genWeight;
+      if( sampleInfo_.isMC ) totWeight *= PUTool->Weight( ntuple->truePU );
+
+      // -- only DY->mumu or DY->ee events according to its name -- //
+      if( DYTool::SelectGenEventBySampleType(sampleInfo_.type, ntuple) )
+      {
+        // -- DY low mass sample: apply NLO/LO k-factor
+        if( sampleInfo_.type.Contains("DYMuMu_M10to50"))
+          totWeight *= nloWeightTool->GetWeight(ntuple);
+
+        vector<DYTool::GenParticle> vec_genMuon = DYTool::GetAllGenLeptons(ntuple, 13, "fromHardProcessFinalState");
+        if( vec_genMuon.size() != 2 ) continue;
+
+        // -- if this event passes the acceptance condition at gen level
+        if( vec_genMuon[0].pt > 20 && fabs(vec_genMuon[0].eta) < 2.4 && 
+            vec_genMuon[1].pt > 20 && fabs(vec_genMuon[1].eta) < 2.4)
+        {
+          vector<DYTool::OffMuon> vec_matchedOffMuon = DYTool::GenMatchedOfflineMuon(ntuple);
+          if( vec_matchedOffMuon.size() != 2 ) continue;
+
+          if( vec_matchedOffMuon[0].pt > 20 && fabs(vec_matchedOffMuon[0].eta) < 2.4 && vec_matchedOffMuon[0].isTRK &&
+              vec_matchedOffMuon[1].pt > 20 && fabs(vec_matchedOffMuon[1].eta) < 2.4 && vec_matchedOffMuon[1].isTRK )
+          {
+            Double_t diMuM = (vec_matchedOffMuon[0].vecP + vec_matchedOffMuon[1].vecP).M();
+
+            Bool_t passTrig = kFALSE;
+            if( IsFired_L1(ntuple) && IsFired_DoubleMu3(ntuple) ) passTrig = kTRUE;
+
+            h_diMuM_DEN->Fill( diMuM, totWeight );
+            if( passTrig ) h_diMuM_NUM->Fill( diMuM, totWeight );
+          } // -- pass acceptance at the reco level
+        } // -- pass acceptance at the gen level
+      } // -- SelectGenEventBySampleType      
+    } // -- end of event iteration
+
+    TString outputName = GetOutputFileName("MakeHist_Dimuon");
+    TFile *f_output = TFile::Open(outputName, "RECREATE");
+    h_diMuM_DEN->Write();
+    h_diMuM_NUM->Write();
+
+    f_output->Close();
+
+    PrintRunTime();
+  }
+
+private:
+  Bool_t IsFired_L1(DYTool::DYTree* ntuple)
+  {
+    Bool_t doPass_L1 = kFALSE;
+    // if( ntuple->vec_L1Bit->at(4) || ntuple->vec_L1Bit->at(16) ) doPass_L1 = kTRUE; // -- no L1 w/ mass cut
+    if( ntuple->vec_L1Bit->at(4) ) doPass_L1 = kTRUE; // -- no L1 w/ mass cut
+
+    return doPass_L1;
+  }
+
+  Bool_t IsFired_DoubleMu3(DYTool::DYTree* ntuple)
+  {
+    Bool_t doPass_HLT = kFALSE;
+    for(const auto& firedTrigger : *(ntuple->vec_firedTrigger) )
+    {
+      TString tstr_firedTrig = firedTrigger;
+      if( tstr_firedTrig.Contains("DST_DoubleMu3_noVtx_CaloScouting_v") ) doPass_HLT = kTRUE;
+
+      if( doPass_HLT ) break;
+    }
+
+    return doPass_HLT;
+  }
+
+
+};
