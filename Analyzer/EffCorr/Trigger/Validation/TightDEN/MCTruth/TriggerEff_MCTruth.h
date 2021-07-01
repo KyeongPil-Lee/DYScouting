7367295cfc7d6e977271e25b2bf8cc7876723395
Kyeongpil Lee <kplee@Kyeongpils-MacBook-Pro.local>
2021-07-01 16:05:58 +0900

    result

---
+++ Analyzer/EffCorr/Trigger/Validation/TightDEN/MCTruth/TriggerEff_MCTruth.h
@@ -0,0 +1,161 @@
+#include <Test/SingleMuon/OfflineHistContainer.h>
+#include <NLOReweight/NLOWeightTool.h>
+
+class HistProducer: public DYTool::ClassTemplate
+{
+public:
+  HistProducer(): ClassTemplate()
+  {
+
+  }
+
+  void Run()
+  {
+    CheckSampleInfo();
+    StartTimer();
+
+    TChain *chain = new TChain("DYTree/ntuple");
+    DYTool::AddNtupleToChain(chain, sampleInfo_.ntuplePathFile);
+
+    DYTool::DYTree *ntuple = new DYTool::DYTree( chain );
+    ntuple->TurnOnBranches_OffMuon();
+
+    Int_t nEvent = chain->GetEntries();
+    cout << "\t[Total Events: " << nEvent << "]" << endl;
+
+    DYTool::PUReweightTool* PUTool = new DYTool::PUReweightTool("2018");
+    NLOWeightTool* nloWeightTool = new NLOWeightTool();
+
+    TH1D* h_diMuM_DEN = new TH1D("h_diMuM_DEN", "", 200, 0, 200);
+    TH1D* h_diMuM_NUM = new TH1D("h_diMuM_NUM", "", 200, 0, 200);
+    TH1D* h_diMuM_NUM_matching = new TH1D("h_diMuM_NUM_matching", "", 200, 0, 200);
+
+    for(Int_t i=0; i<nEvent; i++)
+    {
+      DYTool::loadBar(i+1, nEvent, 100, 100);
+      
+      ntuple->GetEvent(i);
+
+      Double_t genWeight;
+      ntuple->genWeight < 0 ? genWeight = -1 : genWeight = 1;
+
+      Double_t totWeight = sampleInfo_.normFactor * genWeight;
+      // Double_t totWeight = genWeight;
+      if( sampleInfo_.isMC ) totWeight *= PUTool->Weight( ntuple->truePU );
+
+      // -- only DY->mumu or DY->ee events according to its name -- //
+      if( DYTool::SelectGenEventBySampleType(sampleInfo_.type, ntuple) )
+      {
+        // -- DY low mass sample: apply NLO/LO k-factor
+        if( sampleInfo_.type.Contains("DYMuMu_M10to50"))
+          totWeight *= nloWeightTool->GetWeight(ntuple);
+
+        vector<DYTool::GenParticle> vec_genMuon = DYTool::GetAllGenLeptons(ntuple, 13, "fromHardProcessFinalState");
+        if( vec_genMuon.size() != 2 ) continue;
+
+        // -- if this event passes the acceptance condition at gen level
+        if( vec_genMuon[0].pt > 5 && fabs(vec_genMuon[0].eta) < 2.4 && 
+            vec_genMuon[1].pt > 5 && fabs(vec_genMuon[1].eta) < 2.4)
+        {
+          vector<DYTool::OffMuon> vec_matchedOffMuon = DYTool::GenMatchedOfflineMuon(ntuple);
+          if( vec_matchedOffMuon.size() != 2 ) continue;
+
+          if( vec_matchedOffMuon[0].pt > 20 && fabs(vec_matchedOffMuon[0].eta) < 2.4 && vec_matchedOffMuon[0].isTight && vec_matchedOffMuon[0].relPFIso_dBeta < 0.15 &&
+              vec_matchedOffMuon[1].pt > 20 && fabs(vec_matchedOffMuon[1].eta) < 2.4 && vec_matchedOffMuon[1].isTight && vec_matchedOffMuon[0].relPFIso_dBeta < 0.15 )
+          {
+            Double_t diMuM = (vec_matchedOffMuon[0].vecP + vec_matchedOffMuon[1].vecP).M();
+
+            Bool_t passTrig = kFALSE;
+            Bool_t isMatched = kFALSE;
+            if( IsFired_L1(ntuple) && IsFired_DoubleMu3(ntuple) )
+            {
+              passTrig = kTRUE;
+              isMatched = CheckMatching_L1_15_7_HLT_DoubleMu3(ntuple, vec_matchedOffMuon[0], vec_matchedOffMuon[1]);
+            }
+
+            h_diMuM_DEN->Fill( diMuM, totWeight );
+            if( passTrig )              h_diMuM_NUM->Fill( diMuM, totWeight );
+            if( passTrig && isMatched ) h_diMuM_NUM_matching->Fill( diMuM, totWeight ); // -- passTrig is automatically true if isMatched is true, but just express like this to be clear
+
+          } // -- pass acceptance at the reco level
+        } // -- pass acceptance at the gen level
+      } // -- SelectGenEventBySampleType      
+    } // -- end of event iteration
+
+    TString outputName = GetOutputFileName("MakeHist_Dimuon");
+    TFile *f_output = TFile::Open(outputName, "RECREATE");
+
+    h_diMuM_DEN->Write();
+    h_diMuM_NUM->Write();
+    h_diMuM_NUM_matching->Write();
+
+    f_output->Close();
+
+    PrintRunTime();
+  }
+
+private:
+  Bool_t IsFired_L1(DYTool::DYTree* ntuple)
+  {
+    Bool_t doPass_L1 = kFALSE;
+    // if( ntuple->vec_L1Bit->at(4) || ntuple->vec_L1Bit->at(16) ) doPass_L1 = kTRUE; // -- no L1 w/ mass cut
+    if( ntuple->vec_L1Bit->at(4) ) doPass_L1 = kTRUE; // -- no L1 w/ mass cut
+
+    return doPass_L1;
+  }
+
+  Bool_t IsFired_DoubleMu3(DYTool::DYTree* ntuple)
+  {
+    Bool_t doPass_HLT = kFALSE;
+    for(const auto& firedTrigger : *(ntuple->vec_firedTrigger) )
+    {
+      TString tstr_firedTrig = firedTrigger;
+      if( tstr_firedTrig.Contains("DST_DoubleMu3_noVtx_CaloScouting_v") ) doPass_HLT = kTRUE;
+
+      if( doPass_HLT ) break;
+    }
+
+    return doPass_HLT;
+  }
+
+  TString EfficiencyType(DYTool::OffMuon mu, DYTool::DYTree *ntuple)
+  {
+    TString type = "none";
+
+    Double_t minDR = 0.3;
+    Double_t minQuality = 8.0;
+    Double_t minL1Pt = 15.0;
+    Double_t maxL1Pt = 9999.0;
+    Bool_t isHighPtL1Matched = DYTool::dRMatching_L1Muon(mu.vecP_Propagated(), ntuple, minDR, minQuality, minL1Pt, maxL1Pt );
+
+    minL1Pt = 7.0;
+    maxL1Pt = 14.9999;
+    Bool_t isLowPtL1Matched = DYTool::dRMatching_L1Muon(mu.vecP_Propagated(), ntuple, minDR, minQuality, minL1Pt, maxL1Pt );
+
+    Bool_t isHLTMatched = DYTool::dRMatching_HLTObj(mu.vecP, ntuple, "hltDoubleMu3L3FilteredNoVtx", 0.1);
+
+    if( isHLTMatched )
+    {
+      if( isHighPtL1Matched ) type = "highPt";
+      if( isLowPtL1Matched ) type = "lowPt";
+    }
+
+    return type;
+  }
+
+  Bool_t CheckMatching_L1_15_7_HLT_DoubleMu3(DYTool::DYTree* ntuple, DYTool::OffMuon mu1, DYTool::OffMuon mu2)
+  {
+    Bool_t flag = kFALSE;
+
+    TString type1 = EfficiencyType(mu1, ntuple);
+    TString type2 = EfficiencyType(mu2, ntuple);
+
+    if( type1 != "none" && type2 != "none" &&
+        (type1 == "highPt" || type2 == "highPt") ) // -- two muons should be matched (not "none") && at least one muon should be matched to high pT L1 (pT >15 GeV)
+      flag = kTRUE;
+
+    return flag;
+  }
+
+
+};
