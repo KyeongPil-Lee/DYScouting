4c8140319fbdd7b22a8b9ef6b1c2742791b076af
Kyeongpil Lee <kplee@Kyeongpils-MacBook-Pro.local>
2021-07-03 14:51:49 +0900

    calc event eff

---
+++ Analyzer/EffCorr/Trigger/Validation/FineTnPPtBin/SplitLowHighL1/Draw_TnPEff.cxx
@@ -0,0 +1,166 @@
+#include <Include/SimplePlotTools.h>
+#include <EffCorr/Trigger/Validation/TnP/TnPTool.h>
+
+TString PtType(TString fileName)
+{
+  TString type = "";
+  if( fileName.Contains("ROOTFile_MakeTnPHist_HighPtL1andHLT_DYMuMu_M50toInf.root") ) type = "highPtL1";
+  if( fileName.Contains("ROOTFile_MakeTnPHist_LowPtL1andHLT_DYMuMu_M50toInf.root") )  type = "lowPtL1";
+
+  return type;
+}
+
+TGraphAsymmErrors* Graph_TnPEff(TString fileName, TString var)
+{
+  TnPEffTool *tool = new TnPEffTool(fileName);
+  // if( var == "pt" ) var = "highPt";
+  return tool->CalcTnPEff_CutAndCount( var );
+}
+
+void Draw_TnPEff(TString fileName, TString var)
+{
+  TGraphAsymmErrors* g_eff_TnP     = Graph_TnPEff(fileName, var);
+
+  TString ptType = PtType(fileName);
+  TString canvasName = "c_TnPEff_"+var+"_"+ptType;
+
+  Bool_t isLogX = kFALSE;
+  Bool_t isLogY = kFALSE;
+  if( var == "pt" ) isLogX = kTRUE;
+  PlotTool::GraphCanvas* canvas = new PlotTool::GraphCanvas(canvasName, isLogX, isLogY);
+
+  TString titleX = "";
+  if( var == "pt" )  titleX = "p_{T} [GeV]";
+  if( var == "eta" ) titleX = "#eta(#mu)";
+  canvas->SetTitle(titleX, "L1+HLT efficiency");
+
+  // canvas->Register( g_eff_MCTruth, "MC-truth efficiency", kBlack);
+  canvas->Register( g_eff_TnP,     "TnP efficiency",      kBlue);
+
+  if( var == "eta" ) canvas->SetRangeX(-2.4, 2.4);
+  if( var == "eta" )
+  {
+    if( ptType == "highPtL1" ) canvas->SetRangeY(0.76, 1.01);
+    if( ptType == "lowPtL1" )  canvas->SetRangeY(0, 0.07);
+  }
+  if( var == "pt" ) canvas->SetRangeY(0, 1.01);
+  // canvas->SetRangeRatio(0.97, 1.03);
+  // canvas->SetRangeY(0.6, 1.05);
+  canvas->SetLegendPosition(0.50, 0.85, 0.95, 0.95);
+
+  canvas->Latex_CMSInternal();
+  // canvas->RegisterLatex(0.16, 0.91, "#font[42]{#scale[0.6]{IsoMu24 over TightID + PFIso(d#beta)/p_{T}<0.15}}");
+
+  canvas->Draw();
+}
+
+void Print_TnP2DEff(TH2D* h2D)
+{
+  Int_t nBinX = h2D->GetNbinsX();
+  Int_t nBinY = h2D->GetNbinsY();
+
+  for(Int_t i_x=0; i_x<nBinX; i_x++) // -- x axis: pT
+  {
+    Int_t i_binX = i_x+1;
+
+    Double_t ptMin = h2D->GetXaxis()->GetBinLowEdge(i_binX);
+    Double_t ptMax = h2D->GetXaxis()->GetBinLowEdge(i_binX+1);
+
+    printf("[%.1lf < pT < %.1lf]\n", ptMin, ptMax);
+
+    for(Int_t i_y=0; i_y<nBinY; i_y++) // -- y axis: eta
+    {
+      Int_t i_binY = i_y+1;
+
+      Double_t etaMin = h2D->GetYaxis()->GetBinLowEdge(i_binY);
+      Double_t etaMax = h2D->GetYaxis()->GetBinLowEdge(i_binY+1);
+
+      Double_t eff = h2D->GetBinContent(i_binX, i_binY);
+
+      printf("  [%.1lf < eta < %.1lf] -> eff = %.3lf\n", etaMin, etaMax, eff);
+    }
+
+    cout << endl;
+  }
+}
+
+void Draw_TnP2DEff(TString fileName)
+{
+  vector<Double_t> vec_ptEtaBinEdge_pt = {5, 6, 7, 10, 15, 20, 30, 40, 50, 60, 120, 500};
+
+  TnP2DEffTool* tool = new TnP2DEffTool(fileName, vec_ptEtaBinEdge_pt);
+  vector<TGraphAsymmErrors*> vec_effGraph = tool->CalcTnPEff_CutAndCount("pteta");
+  TH2D* h2D_eff = tool->Get_2DEffMap();
+
+  // -- drawing 2D histogram
+  TString ptType = PtType(fileName);
+  TString canvasName = "c_TnP2DEff_"+ptType;
+  PlotTool::Hist2DCanvas *canvas = new PlotTool::Hist2DCanvas(canvasName, 1, 0, 0);
+  canvas->SetTitle("p_{T} [GeV]", "#eta");
+
+  canvas->Register( h2D_eff );
+  canvas->SetRangeZ(0, 1.01);
+  canvas->Latex_CMSInternal();
+
+  canvas->Draw("COLZ");
+  // canvas->Draw("COLZTEXT");
+
+  Print_TnP2DEff(h2D_eff);
+}
+
+void Draw_TnPEff(TString fileName)
+{
+  Draw_TnPEff(fileName, "pt");
+  Draw_TnPEff(fileName, "eta");
+  Draw_TnP2DEff(fileName);
+}
+
+void Draw_TnPEff()
+{
+  TString fileName_highPt = "HighPt/ROOTFile_MakeTnPHist_HighPtL1andHLT_DYMuMu_M50toInf.root";
+  TString fileName_lowPt  = "LowPt/ROOTFile_MakeTnPHist_LowPtL1andHLT_DYMuMu_M50toInf.root";
+
+  Draw_TnPEff(fileName_highPt);
+  Draw_TnPEff(fileName_lowPt);
+
+  // -- make a root file with 2D map histogram
+  vector<Double_t> vec_ptEtaBinEdge_pt = {5, 6, 7, 10, 15, 20, 30, 40, 50, 60, 120, 500};
+  TnP2DEffTool* tool_lowPt = new TnP2DEffTool(fileName_lowPt, vec_ptEtaBinEdge_pt);
+  vector<TGraphAsymmErrors*> vec_effGraph_lowPt = tool_lowPt->CalcTnPEff_CutAndCount("pteta");
+  TH2D* h2D_eff_lowPt = tool_lowPt->Get_2DEffMap();
+  h2D_eff_lowPt->SetName("h2D_eff_lowPt");
+
+  TnP2DEffTool* tool_highPt = new TnP2DEffTool(fileName_highPt, vec_ptEtaBinEdge_pt);
+  vector<TGraphAsymmErrors*> vec_effGraph_highPt = tool_highPt->CalcTnPEff_CutAndCount("pteta");
+  TH2D* h2D_eff_highPt = tool_highPt->Get_2DEffMap();
+  h2D_eff_highPt->SetName("h2D_eff_highPt");
+
+  // -- 1D histograms
+  TGraphAsymmErrors* g_eff_pt_lowPt  = Graph_TnPEff(fileName_lowPt, "pt");
+  TGraphAsymmErrors* g_eff_eta_lowPt = Graph_TnPEff(fileName_lowPt, "eta");
+
+  TH1D* h_eff_pt_lowPt  = PlotTool::Convert_GraphToHist(g_eff_pt_lowPt);
+  TH1D* h_eff_eta_lowPt = PlotTool::Convert_GraphToHist(g_eff_eta_lowPt);
+  h_eff_pt_lowPt->SetName("h_eff_pt_lowPt");
+  h_eff_eta_lowPt->SetName("h_eff_eta_lowPt");
+
+  TGraphAsymmErrors* g_eff_pt_highPt  = Graph_TnPEff(fileName_highPt, "pt");
+  TGraphAsymmErrors* g_eff_eta_highPt = Graph_TnPEff(fileName_highPt, "eta");
+
+  TH1D* h_eff_pt_highPt  = PlotTool::Convert_GraphToHist(g_eff_pt_highPt);
+  TH1D* h_eff_eta_highPt = PlotTool::Convert_GraphToHist(g_eff_eta_highPt);
+  h_eff_pt_highPt->SetName("h_eff_pt_highPt");
+  h_eff_eta_highPt->SetName("h_eff_eta_highPt");
+
+  TFile* f_output = TFile::Open("ROOTFile_L1HLTLegEfficiencyMap.root", "RECREATE");
+  h2D_eff_lowPt->Write();
+  h2D_eff_highPt->Write();
+
+  h_eff_pt_lowPt->Write();
+  h_eff_eta_lowPt->Write();
+
+  h_eff_pt_highPt->Write();
+  h_eff_eta_highPt->Write();
+
+  f_output->Close();
+}
