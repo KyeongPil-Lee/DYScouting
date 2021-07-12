0996b1642ed775f6f252f1012e3252fc4492e7e2
Kyeongpil Lee <kplee@Kyeongpils-MacBook-Pro.local>
2021-07-12 12:53:45 +0900

    add a few more histograms

--- Analyzer/EffCorr/Trigger/Validation/MCTruthLegEff/MakeHist_MCTruthLegEff.h
+++ Analyzer/EffCorr/Trigger/Validation/MCTruthLegEff/MakeHist_MCTruthLegEff.h
@@ -27,12 +27,20 @@ public:
 
     vector<Double_t> vec_ptBinEdge     = {0, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 24, 26, 30, 35, 40, 50, 60, 120, 200, 500};
     vector<Double_t> vec_highPtBinEdge = {0, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 14, 15, 16, 17, 18, 19, 20, 22, 24, 26, 30, 40, 50, 60, 120, 200, 500};
+    vector<Double_t> vec_etaBinEdge    = {-2.4, -2.1, -1.6, -1.2, -0.9, -0.3, -0.2, 0, 0.2, 0.3, 0.9, 1.2, 1.6, 2.1, 2.4};
 
     TH1D* h_mu_pt_lowPtBin_all     = DYTool::MakeTH1D_BinEdgeVector("h_mu_pt_lowPtBin_all",     vec_ptBinEdge);
     TH1D* h_mu_pt_lowPtBin_matched = DYTool::MakeTH1D_BinEdgeVector("h_mu_pt_lowPtBin_matched", vec_ptBinEdge);
+    TH1D* h_mu_eta_lowPtBin_all     = DYTool::MakeTH1D_BinEdgeVector("h_mu_eta_lowPtBin_all",     vec_etaBinEdge);
+    TH1D* h_mu_eta_lowPtBin_matched = DYTool::MakeTH1D_BinEdgeVector("h_mu_eta_lowPtBin_matched", vec_etaBinEdge);
 
     TH1D* h_mu_pt_highPtBin_all     = DYTool::MakeTH1D_BinEdgeVector("h_mu_pt_highPtBin_all",     vec_highPtBinEdge);
     TH1D* h_mu_pt_highPtBin_matched = DYTool::MakeTH1D_BinEdgeVector("h_mu_pt_highPtBin_matched", vec_highPtBinEdge);
+    TH1D* h_mu_eta_highPtBin_all     = DYTool::MakeTH1D_BinEdgeVector("h_mu_eta_highPtBin_all",     vec_etaBinEdge);
+    TH1D* h_mu_eta_highPtBin_matched = DYTool::MakeTH1D_BinEdgeVector("h_mu_eta_highPtBin_matched", vec_etaBinEdge);
+
+    TH1D* h_mu_pt_lowPtBin_L1Matched = DYTool::MakeTH1D_BinEdgeVector("h_mu_pt_lowPtBin_L1Matched", vec_ptBinEdge);
+    TH1D* h_mu_pt_highPtBin_L1Matched = DYTool::MakeTH1D_BinEdgeVector("h_mu_pt_highPtBin_L1Matched", vec_highPtBinEdge);
 
     for(Int_t i=0; i<nEvent; i++)
     {
@@ -63,10 +71,24 @@ public:
           {
             h_mu_pt_lowPtBin_all->Fill( offMuon.pt, totWeight );
             h_mu_pt_highPtBin_all->Fill( offMuon.pt, totWeight );
+            h_mu_eta_lowPtBin_all->Fill( offMuon.eta, totWeight );
+            h_mu_eta_highPtBin_all->Fill( offMuon.eta, totWeight );
+
+            // -- only L1 check
+            if( IsMatched_LowPtL1(offMuon, ntuple) )  h_mu_pt_lowPtBin_L1Matched->Fill( offMuon.pt, totWeight );
+            if( IsMatched_HighPtL1(offMuon, ntuple) ) h_mu_pt_highPtBin_L1Matched->Fill( offMuon.pt, totWeight );
 
             TString matchingType = EfficiencyType(offMuon, ntuple);
-            if( matchingType == "lowPt" )  h_mu_pt_lowPtBin_matched->Fill( offMuon.pt, totWeight );
-            if( matchingType == "highPt" ) h_mu_pt_highPtBin_matched->Fill( offMuon.pt, totWeight );
+            if( matchingType == "lowPt" ) 
+            {
+              h_mu_pt_lowPtBin_matched->Fill( offMuon.pt, totWeight );
+              h_mu_eta_lowPtBin_matched->Fill( offMuon.eta, totWeight );
+            }
+            if( matchingType == "highPt" )
+            {
+              h_mu_pt_highPtBin_matched->Fill( offMuon.pt, totWeight );
+              h_mu_eta_highPtBin_matched->Fill( offMuon.eta, totWeight );
+            }
           }
 
         } // -- end of loop over offline muons
@@ -78,9 +100,16 @@ public:
 
     h_mu_pt_lowPtBin_all->Write();
     h_mu_pt_lowPtBin_matched->Write();
+    h_mu_eta_lowPtBin_all->Write();
+    h_mu_eta_lowPtBin_matched->Write();
 
     h_mu_pt_highPtBin_all->Write();
     h_mu_pt_highPtBin_matched->Write();
+    h_mu_eta_highPtBin_all->Write();
+    h_mu_eta_highPtBin_matched->Write();
+
+    h_mu_pt_lowPtBin_L1Matched->Write();
+    h_mu_pt_highPtBin_L1Matched->Write();
 
     f_output->Close();
 
@@ -134,6 +163,35 @@ private:
     }
 
     return type;
+  }
+
+  Bool_t IsMatched_HighPtL1(DYTool::OffMuon mu, DYTool::DYTree *ntuple)
+  {
+    Double_t minDR = 0.3;
+    Double_t minQuality = 8.0;
+    Double_t minL1Pt = 15.0;
+    Double_t maxL1Pt = 9999.0;
+    Bool_t isHighPtL1Matched = DYTool::dRMatching_L1Muon(mu.vecP_Propagated(), ntuple, minDR, minQuality, minL1Pt, maxL1Pt );
+
+    return isHighPtL1Matched;
+  }
+
+  Bool_t IsMatched_LowPtL1(DYTool::OffMuon mu, DYTool::DYTree *ntuple)
+  {
+    Double_t minDR = 0.3;
+    Double_t minQuality = 8.0;
+    Double_t minL1Pt = 7.0;
+    Double_t maxL1Pt = 14.9999;
+    Bool_t isLowPtL1Matched = DYTool::dRMatching_L1Muon(mu.vecP_Propagated(), ntuple, minDR, minQuality, minL1Pt, maxL1Pt );
+
+    return isLowPtL1Matched;
+  }
+
+  Bool_t IsMatched_HLTObj(DYTool::OffMuon mu, DYTool::DYTree *ntuple)
+  {
+    Bool_t isHLTMatched = DYTool::dRMatching_HLTObj(mu.vecP, ntuple, "hltDoubleMu3L3FilteredNoVtx", 0.1);
+
+    return isHLTMatched;
   }
 
 };
