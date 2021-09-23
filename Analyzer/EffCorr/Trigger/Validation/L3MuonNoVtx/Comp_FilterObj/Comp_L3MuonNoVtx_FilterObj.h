d1a0905d6cebe86db2422a32968eef230bdd65f3
Kyeongpil Lee <kplee@Kyeongpils-MacBook-Pro.local>
2021-09-23 17:23:37 +0200

    add further histograms

--- Analyzer/EffCorr/Trigger/Validation/L3MuonNoVtx/Comp_FilterObj/Comp_L3MuonNoVtx_FilterObj.h
+++ Analyzer/EffCorr/Trigger/Validation/L3MuonNoVtx/Comp_FilterObj/Comp_L3MuonNoVtx_FilterObj.h
@@ -26,17 +26,65 @@ public:
     DYTool::PUReweightTool* PUTool = new DYTool::PUReweightTool("2018");
     NLOWeightTool* nloWeightTool = new NLOWeightTool();
 
-    TH1D* h_nFilterObj       = new TH1D("h_nFilterObj",       "", 100, 0, 100);
-    TH1D* h_nL3MuonNoVtx_pt3 = new TH1D("h_nL3MuonNoVtx_pt3", "", 100, 0, 100);
+    // -- # events of its type according to the generator level variables
+    // -- nFilterObj == nL3MuonNoVtx
+    // -- nFilterObj > nL3MuonNoVtx: my custom filter rejects more
+    // -- nFilterObj < nL3MuonNoVtx: my custom filter couldn't reject the muons
+    TH1D* h_nEvent_diMuPt_tot   = new TH1D("h_nEvent_diMuPt_tot",   "", 10000, 0, 10000);
+    TH1D* h_nEvent_diMuPt_type1 = new TH1D("h_nEvent_diMuPt_type1", "", 10000, 0, 10000);
+    TH1D* h_nEvent_diMuPt_type2 = new TH1D("h_nEvent_diMuPt_type2", "", 10000, 0, 10000);
+    TH1D* h_nEvent_diMuPt_type3 = new TH1D("h_nEvent_diMuPt_type3", "", 10000, 0, 10000);
 
-    TH1D* h_pt_filterObj   = new TH1D("h_pt_filterObj",   "", 10000, 0, 10000);
-    TH1D* h_pt_L3MuonNoVtx = new TH1D("h_pt_L3MuonNoVtx", "", 10000, 0, 10000);
+    TH1D* h_nEvent_diMuM_tot   = new TH1D("h_nEvent_diMuM_tot",   "", 10000, 0, 10000);
+    TH1D* h_nEvent_diMuM_type1 = new TH1D("h_nEvent_diMuM_type1", "", 10000, 0, 10000);
+    TH1D* h_nEvent_diMuM_type2 = new TH1D("h_nEvent_diMuM_type2", "", 10000, 0, 10000);
+    TH1D* h_nEvent_diMuM_type3 = new TH1D("h_nEvent_diMuM_type3", "", 10000, 0, 10000);
 
-    TH1D* h_eta_filterObj   = new TH1D("h_eta_filterObj",   "", 60, -3, 3);
-    TH1D* h_eta_L3MuonNoVtx = new TH1D("h_eta_L3MuonNoVtx", "", 60, -3, 3);
+    TH1D* h_nEvent_diMuRap_tot   = new TH1D("h_nEvent_diMuRap_tot",   "", 200, -10, 10);
+    TH1D* h_nEvent_diMuRap_type1 = new TH1D("h_nEvent_diMuRap_type1", "", 200, -10, 10);
+    TH1D* h_nEvent_diMuRap_type2 = new TH1D("h_nEvent_diMuRap_type2", "", 200, -10, 10);
+    TH1D* h_nEvent_diMuRap_type3 = new TH1D("h_nEvent_diMuRap_type3", "", 200, -10, 10);
 
-    TH1D* h_phi_filterObj   = new TH1D("h_phi_filterObj",   "", 80, -4, 4);
-    TH1D* h_phi_L3MuonNoVtx = new TH1D("h_phi_L3MuonNoVtx", "", 80, -4, 4);
+
+    // -- histogram for tot
+    TH1D* h_pt_filterObj_tot   = new TH1D("h_pt_filterObj_tot",   "", 10000, 0, 10000);
+    TH1D* h_pt_L3MuonNoVtx_tot = new TH1D("h_pt_L3MuonNoVtx_tot", "", 10000, 0, 10000);
+
+    TH1D* h_eta_filterObj_tot   = new TH1D("h_eta_filterObj_tot",   "", 60, -3, 3);
+    TH1D* h_eta_L3MuonNoVtx_tot = new TH1D("h_eta_L3MuonNoVtx_tot", "", 60, -3, 3);
+
+    TH1D* h_phi_filterObj_tot   = new TH1D("h_phi_filterObj_tot",   "", 80, -4, 4);
+    TH1D* h_phi_L3MuonNoVtx_tot = new TH1D("h_phi_L3MuonNoVtx_tot", "", 80, -4, 4);
+
+    // -- histogram for type1
+    TH1D* h_pt_filterObj_type1   = new TH1D("h_pt_filterObj_type1",   "", 10000, 0, 10000);
+    TH1D* h_pt_L3MuonNoVtx_type1 = new TH1D("h_pt_L3MuonNoVtx_type1", "", 10000, 0, 10000);
+
+    TH1D* h_eta_filterObj_type1   = new TH1D("h_eta_filterObj_type1",   "", 60, -3, 3);
+    TH1D* h_eta_L3MuonNoVtx_type1 = new TH1D("h_eta_L3MuonNoVtx_type1", "", 60, -3, 3);
+
+    TH1D* h_phi_filterObj_type1   = new TH1D("h_phi_filterObj_type1",   "", 80, -4, 4);
+    TH1D* h_phi_L3MuonNoVtx_type1 = new TH1D("h_phi_L3MuonNoVtx_type1", "", 80, -4, 4);
+
+    // -- histogram for type2
+    TH1D* h_pt_filterObj_type2   = new TH1D("h_pt_filterObj_type2",   "", 10000, 0, 10000);
+    TH1D* h_pt_L3MuonNoVtx_type2 = new TH1D("h_pt_L3MuonNoVtx_type2", "", 10000, 0, 10000);
+
+    TH1D* h_eta_filterObj_type2   = new TH1D("h_eta_filterObj_type2",   "", 60, -3, 3);
+    TH1D* h_eta_L3MuonNoVtx_type2 = new TH1D("h_eta_L3MuonNoVtx_type2", "", 60, -3, 3);
+
+    TH1D* h_phi_filterObj_type2   = new TH1D("h_phi_filterObj_type2",   "", 80, -4, 4);
+    TH1D* h_phi_L3MuonNoVtx_type2 = new TH1D("h_phi_L3MuonNoVtx_type2", "", 80, -4, 4);
+
+    // -- histogram for type3
+    TH1D* h_pt_filterObj_type3   = new TH1D("h_pt_filterObj_type3",   "", 10000, 0, 10000);
+    TH1D* h_pt_L3MuonNoVtx_type3 = new TH1D("h_pt_L3MuonNoVtx_type3", "", 10000, 0, 10000);
+
+    TH1D* h_eta_filterObj_type3   = new TH1D("h_eta_filterObj_type3",   "", 60, -3, 3);
+    TH1D* h_eta_L3MuonNoVtx_type3 = new TH1D("h_eta_L3MuonNoVtx_type3", "", 60, -3, 3);
+
+    TH1D* h_phi_filterObj_type3   = new TH1D("h_phi_filterObj_type3",   "", 80, -4, 4);
+    TH1D* h_phi_L3MuonNoVtx_type3 = new TH1D("h_phi_L3MuonNoVtx_type3", "", 80, -4, 4);
 
     Int_t i_event = 0;
     for(Int_t i=0; i<nEvent; i++)
@@ -61,6 +109,19 @@ public:
 
         vector<DYTool::GenParticle> vec_genMuon = DYTool::GetAllGenLeptons(ntuple, 13, "fromHardProcessFinalState");
         if( vec_genMuon.size() != 2 ) continue;
+
+        // -- apply acceptance at the generator level: let's consider the events within the detector acceptance only
+        Bool_t flag_passGenAcc = kFALSE;
+        if( vec_genMuon[0].pt > 5 && vec_genMuon[0].eta < 2.4 &&
+            vec_genMuon[1].pt > 5 && vec_genMuon[1].eta < 2.4 ) flag_passGenAcc = kTRUE;
+
+        if( !flag_passGenAcc ) continue;
+
+        TLorentzVector vecP_diMu_gen = vec_genMuon[0].vecP + vec_genMuon[1].vecP;
+
+        Double_t diMuPt_gen  = vecP_diMu_gen.Pt();
+        Double_t diMuM_gen   = vecP_diMu_gen.M();
+        Double_t diMuRap_gen = vecP_diMu_gen.Rapidity();
 
         // i_event++;
 
@@ -93,30 +154,101 @@ public:
         vector<DYTool::HLTObj> vec_filterObj = RemoveDuplicatedObject( vec_filterObj_temp );
 
         vector<DYTool::L3MuonNoVtx> vec_L3MuonNoVtx_pt3;
-        // Bool_t doPass_customFilter = CustomFilter_DoubleMu3(ntuple, vec_L3MuonNoVtx_pt3);
         Bool_t doPass_customFilter = CustomFilter_DoubleMu3(ntuple, vec_L3MuonNoVtx_pt3);
 
-        Int_t nFilterObj       = (Int_t)vec_filterObj.size();
-        Int_t nL3MuonNoVtx_pt3 = (Int_t)vec_L3MuonNoVtx_pt3.size();
+        Int_t nFilterObj   = (Int_t)vec_filterObj.size();
+        Int_t nL3MuonNoVtx = (Int_t)vec_L3MuonNoVtx_pt3.size();
+
+        h_nEvent_diMuPt_tot->Fill( diMuPt_gen, totWeight );
+        h_nEvent_diMuM_tot->Fill( diMuM_gen, totWeight );
+        h_nEvent_diMuRap_tot->Fill( diMuRap_gen, totWeight );
+
+        Bool_t isType1 = kFALSE;
+        Bool_t isType2 = kFALSE;
+        Bool_t isType3 = kFALSE;
+
+        if( nFilterObj == nL3MuonNoVtx )      isType1 = kTRUE;
+        else if( nFilterObj > nL3MuonNoVtx )  isType2 = kTRUE;
+        else if( nFilterObj < nL3MuonNoVtx )  isType3 = kTRUE;
 
         h_nFilterObj->Fill( nFilterObj, totWeight );
-        h_nL3MuonNoVtx_pt3->Fill( nL3MuonNoVtx_pt3, totWeight );
+        h_nL3MuonNoVtx->Fill( nL3MuonNoVtx, totWeight );
 
+        if( isType1 )
+        {
+          h_nEvent_diMuPt_type1->Fill( diMuPt_gen, totWeight );
+          h_nEvent_diMuM_type1->Fill( diMuM_gen, totWeight );
+          h_nEvent_diMuRap_type1->Fill( diMuRap_gen, totWeight );
+        }
+        if( isType2 )
+        {
+          h_nEvent_diMuPt_type2->Fill( diMuPt_gen, totWeight );
+          h_nEvent_diMuM_type2->Fill( diMuM_gen, totWeight );
+          h_nEvent_diMuRap_type2->Fill( diMuRap_gen, totWeight );
+        }
+        if( isType3 )
+        {
+          h_nEvent_diMuPt_type3->Fill( diMuPt_gen, totWeight );
+          h_nEvent_diMuM_type3->Fill( diMuM_gen, totWeight );
+          h_nEvent_diMuRap_type3->Fill( diMuRap_gen, totWeight );
+        }
+
+        // -- kinematics for the filter objects
         for(auto& filterObj : vec_filterObj )
         {
-          h_pt_filterObj->Fill( filterObj.pt, totWeight );
-          h_eta_filterObj->Fill( filterObj.eta, totWeight );
-          h_phi_filterObj->Fill( filterObj.phi, totWeight );
+          h_pt_filterObj_tot->Fill( filterObj.pt, totWeight );
+          h_eta_filterObj_tot->Fill( filterObj.eta, totWeight );
+          h_phi_filterObj_tot->Fill( filterObj.phi, totWeight );
+
+          if( isType1 )
+          {
+            h_pt_filterObj_type1->Fill( filterObj.pt, totWeight );
+            h_eta_filterObj_type1->Fill( filterObj.eta, totWeight );
+            h_phi_filterObj_type1->Fill( filterObj.phi, totWeight );
+          }
+          if( isType2 )
+          {
+            h_pt_filterObj_type2->Fill( filterObj.pt, totWeight );
+            h_eta_filterObj_type2->Fill( filterObj.eta, totWeight );
+            h_phi_filterObj_type2->Fill( filterObj.phi, totWeight );
+          }
+          if( isType3 )
+          {
+            h_pt_filterObj_type3->Fill( filterObj.pt, totWeight );
+            h_eta_filterObj_type3->Fill( filterObj.eta, totWeight );
+            h_phi_filterObj_type3->Fill( filterObj.phi, totWeight );
+          }
         }
 
         for(auto& L3MuonNoVtx : vec_L3MuonNoVtx_pt3 )
         {
-          h_pt_L3MuonNoVtx->Fill( L3MuonNoVtx.pt, totWeight );
-          h_eta_L3MuonNoVtx->Fill( L3MuonNoVtx.eta, totWeight );
-          h_phi_L3MuonNoVtx->Fill( L3MuonNoVtx.phi, totWeight );
+          h_pt_L3MuonNoVtx_tot->Fill( L3MuonNoVtx.pt, totWeight );
+          h_eta_L3MuonNoVtx_tot->Fill( L3MuonNoVtx.eta, totWeight );
+          h_phi_L3MuonNoVtx_tot->Fill( L3MuonNoVtx.phi, totWeight );
+
+          if( isType1 )
+          {
+            h_pt_L3MuonNoVtx_type1->Fill( L3MuonNoVtx.pt, totWeight );
+            h_eta_L3MuonNoVtx_type1->Fill( L3MuonNoVtx.eta, totWeight );
+            h_phi_L3MuonNoVtx_type1->Fill( L3MuonNoVtx.phi, totWeight );
+          }
+          if( isType2 )
+          {
+            h_pt_L3MuonNoVtx_type2->Fill( L3MuonNoVtx.pt, totWeight );
+            h_eta_L3MuonNoVtx_type2->Fill( L3MuonNoVtx.eta, totWeight );
+            h_phi_L3MuonNoVtx_type2->Fill( L3MuonNoVtx.phi, totWeight );
+          }
+          if( isType3 )
+          {
+            h_pt_L3MuonNoVtx_type3->Fill( L3MuonNoVtx.pt, totWeight );
+            h_eta_L3MuonNoVtx_type3->Fill( L3MuonNoVtx.eta, totWeight );
+            h_phi_L3MuonNoVtx_type3->Fill( L3MuonNoVtx.phi, totWeight );
+          }
         }
 
-        if( nFilterObj != nL3MuonNoVtx_pt3 )
+
+        // -- logging
+        if( nFilterObj != nL3MuonNoVtx )
         {
           TString basePath = "/data9/Users/kplee/Physics/DYScouting/Analyzer/EffCorr/Trigger/Validation/L3MuonNoVtx/Comp_FilterObj";
           ofstream logEvent(basePath+"/Event_differentNumberOfObject.txt", std::ios_base::out | std::ios_base::app);
@@ -145,7 +277,7 @@ public:
           logEvent << endl;
 
           // -- when the custom filter reject more than the real filter: trace the rejected objects to understand the reason
-          if( nFilterObj > nL3MuonNoVtx_pt3 )
+          if( nFilterObj > nL3MuonNoVtx )
           {
             vector<DYTool::L3MuonNoVtx> vec_filterObj_singleMu_debug;
             Bool_t flag_singleMu = CustomSingleMuFilter_MimicDoubleMu3Leg_Debug(ntuple, vec_filterObj_singleMu_debug, logEvent);
@@ -164,15 +296,57 @@ public:
     TFile *f_output = TFile::Open(outputName, "RECREATE");
 
     h_nFilterObj->Write();
-    h_nL3MuonNoVtx_pt3->Write();
+    h_nL3MuonNoVtx->Write();
 
-    h_pt_filterObj->Write();
-    h_eta_filterObj->Write();
-    h_phi_filterObj->Write();
 
-    h_pt_L3MuonNoVtx->Write();
-    h_eta_L3MuonNoVtx->Write();
-    h_phi_L3MuonNoVtx->Write();
+    h_nEvent_diMuPt_tot->Write();
+    h_nEvent_diMuPt_type1->Write();
+    h_nEvent_diMuPt_type2->Write();
+    h_nEvent_diMuPt_type3->Write();
+
+    h_nEvent_diMuM_tot->Write();
+    h_nEvent_diMuM_type1->Write();
+    h_nEvent_diMuM_type2->Write();
+    h_nEvent_diMuM_type3->Write();
+
+    h_nEvent_diMuRap_tot->Write();
+    h_nEvent_diMuRap_type1->Write();
+    h_nEvent_diMuRap_type2->Write();
+    h_nEvent_diMuRap_type3->Write();
+
+
+    h_pt_filterObj_tot->Write();
+    h_eta_filterObj_tot->Write();
+    h_phi_filterObj_tot->Write();
+
+    h_pt_filterObj_type1->Write();
+    h_eta_filterObj_type1->Write();
+    h_phi_filterObj_type1->Write();
+
+    h_pt_filterObj_type2->Write();
+    h_eta_filterObj_type2->Write();
+    h_phi_filterObj_type2->Write();
+
+    h_pt_filterObj_type3->Write();
+    h_eta_filterObj_type3->Write();
+    h_phi_filterObj_type3->Write();
+
+
+    h_pt_L3MuonNoVtx_tot->Write();
+    h_eta_L3MuonNoVtx_tot->Write();
+    h_phi_L3MuonNoVtx_tot->Write();
+
+    h_pt_L3MuonNoVtx_type1->Write();
+    h_eta_L3MuonNoVtx_type1->Write();
+    h_phi_L3MuonNoVtx_type1->Write();
+
+    h_pt_L3MuonNoVtx_type2->Write();
+    h_eta_L3MuonNoVtx_type2->Write();
+    h_phi_L3MuonNoVtx_type2->Write();
+
+    h_pt_L3MuonNoVtx_type3->Write();
+    h_eta_L3MuonNoVtx_type3->Write();
+    h_phi_L3MuonNoVtx_type3->Write();
 
     f_output->Close();
 
--- Analyzer/EffCorr/Trigger/Validation/L3MuonNoVtx/Comp_FilterObj/JobConfig.py
+++ Analyzer/EffCorr/Trigger/Validation/L3MuonNoVtx/Comp_FilterObj/JobConfig.py
@@ -21,7 +21,7 @@ generator.jsonName = "SampleInfo_v1p7.json" # -- under DY_ANALYZER_PATH/Include
 
 # -- if "_skim" is added, it runs over skimmed ntuples
 generator.dic_nJob = { 
-  # "DYMuMu_M10to50":    10, 
+  "DYMuMu_M10to50":    10, 
   "DYMuMu_M50toInf":   30,
   }
 
